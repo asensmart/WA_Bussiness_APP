5d41c02a3f1d1f44794527fb75720a3d0f13c9fc
asensmart <asenali.g@dyooti.com>
2025-09-14 01:32:27 +0530

    Update code

--- Common/common.js
+++ Common/common.js
@@ -1,4 +1,6 @@
 var axios = require("axios");
+const FormData = require("form-data");
+const { fileTypeFromFile } = require("file-type");
 
 module.exports = {
   sendWhatsAppMessage: async function () {
@@ -24,6 +26,184 @@ module.exports = {
       });
 
       console.log("Message sent:", response.data);
+      return response;
+    } catch (error) {
+      console.error(
+        "Error sending message:",
+        error.response?.data || error.message
+      );
+    }
+  },
+  sendCustomWhatsAppMessage: async function () {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: process.env.WA_URL,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WA_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: "918838497173",
+          type: "text",
+          text: {
+            body: "Hello, this is a custom message!",
+          },
+        },
+      });
+
+      console.log("Message sent:", response.data);
+      return response;
+    } catch (error) {
+      console.error(
+        "Error sending message:",
+        error.response?.data || error.message
+      );
+    }
+  },
+  sendImageWhatsAppMessage: async function () {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: process.env.WA_URL,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WA_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: "918838497173",
+          type: "image",
+          image: {
+            link: "https://dummyimage.com/600x400/000/fff.png&text=Allahu+Akbar",
+            // id: "801354005688179",
+            caption: "This is a sample image",
+          },
+        },
+      });
+
+      console.log("Message sent:", response.data);
+      return response;
+    } catch (error) {
+      console.error(
+        "Error sending message:",
+        error.response?.data || error.message
+      );
+    }
+  },
+  sendAudioWhatsAppMessage: async function () {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: process.env.WA_URL,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WA_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: "918838497173",
+          type: "audio",
+          audio: {
+            link: "https://file-examples.com/storage/feb23bcbd368c1ded9d9b0e/2017/11/file_example_MP3_2MG.mp3",
+          },
+        },
+      });
+
+      console.log("Message sent:", response.data);
+      return response;
+    } catch (error) {
+      console.error(
+        "Error sending message:",
+        error.response?.data || error.message
+      );
+    }
+  },
+  sendVideoWhatsAppMessage: async function () {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: process.env.WA_URL,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WA_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: "918838497173",
+          type: "video",
+          video: {
+            link: "https://file-examples.com/storage/feb23bcbd368c1ded9d9b0e/2017/04/file_example_MP4_480_1_5MG.mp4",
+          },
+        },
+      });
+
+      console.log("Message sent:", response.data);
+      return response;
+    } catch (error) {
+      console.error(
+        "Error sending message:",
+        error.response?.data || error.message
+      );
+    }
+  },
+
+  //  Send meadia files
+  sendMediaWhatsAppfiles: async function (filePath) {
+    try {
+      let mediaType = await fileTypeFromFile(filePath);
+
+      console.log("File Extension:", mediaType?.mime);
+
+      const data = new FormData();
+      data.append("messaging_product", "whatsapp");
+      data.append("file", fs.createReadStream(filePath), {
+        contentType: mediaType?.mime,
+      });
+      data.append("type", mediaType?.mime);
+
+      const response = await axios({
+        method: "POST",
+        url: process.env.WA_MEDIA_URL,
+        headers: {
+          Authorization: `Bearer ${process.env.WA_TOKEN}`,
+        },
+        data: data,
+      });
+
+      console.log("Message sent:", response.data);
+      return response;
+    } catch (error) {
+      console.error(
+        "Error sending message:",
+        error.response?.data || error.message
+      );
+    }
+  },
+  sendImageWhatsAppIDMessage: async function (fileId, fileType) {
+    try {
+      console.log("fielId, fileType", fileId, fileType);
+
+      const response = await axios({
+        method: "POST",
+        url: process.env.WA_URL,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.WA_TOKEN}`,
+        },
+        data: {
+          messaging_product: "whatsapp",
+          to: "918838497173",
+          type: fileType, //image, audio, video
+          [fileType]: {
+            id: fileId,
+            caption: "This is a sample image with id",
+          },
+        },
+      });
+
+      console.log("Message sent: sendImageWhatsAppIDMessage", response.data);
       return response;
     } catch (error) {
       console.error(
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 require("dotenv").config(); // Load environment variables from .env file
 const express = require("express");
 const app = express();
-const { sendWhatsAppMessage } = require("./Common/common");
+const {
+  sendWhatsAppMessage,
+  sendCustomWhatsAppMessage,
+  sendImageWhatsAppMessage,
+  sendAudioWhatsAppMessage,
+  sendVideoWhatsAppMessage,
+  sendMediaWhatsAppfiles,
+  sendImageWhatsAppIDMessage,
+} = require("./Common/common");
+const fs = require("fs");
+const path = require("path");
+const { fileTypeFromFile } = require("file-type");
+const { get } = require("http");
+
+// console.log(
+//   "webp image -->" + fs.readFileSync(process.cwd() + "/media/test.webp")
+// );
 
 var port = process.env.PORT || 3000; // Use PORT from environment or default to 3000
 console.log(`Server will run on port: ${port}`);
 
-app.get("/webhook", (req, res) => {
+app.get("/sendTemplateMessage", (req, res) => {
   sendWhatsAppMessage()
     .then((getData) => {
       res.json({
@@ -17,6 +33,122 @@ app.get("/webhook", (req, res) => {
     })
     .catch((error) => {
       console.error("Error in sendWhatsAppMessage:", error);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to send WhatsApp message.",
+        error: error.message,
+      });
+    });
+});
+
+app.get("/sendCustomMessage", (req, res) => {
+  sendCustomWhatsAppMessage()
+    .then((getData) => {
+      res.json({
+        status: "success",
+        message: "Webhook received and data fetch initiated.",
+        data: getData.data.messages,
+      });
+    })
+    .catch((error) => {
+      console.error("Error in sendCustomWhatsAppMessage:", error);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to send WhatsApp message.",
+        error: error.message,
+      });
+    });
+});
+
+app.get("/sendImageWhatsAppMessage", (req, res) => {
+  sendImageWhatsAppMessage()
+    .then((getData) => {
+      res.json({
+        status: "success",
+        message: "Webhook received and data fetch initiated.",
+        data: getData.data.messages,
+      });
+    })
+    .catch((error) => {
+      console.error("Error in sendImageWhatsAppMessage:", error);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to send WhatsApp message.",
+        error: error.message,
+      });
+    });
+});
+
+app.get("/sendAudioWhatsAppMessage", (req, res) => {
+  sendAudioWhatsAppMessage()
+    .then((getData) => {
+      res.json({
+        status: "success",
+        message: "Webhook received and data fetch initiated.",
+        data: getData.data.messages,
+      });
+    })
+    .catch((error) => {
+      console.error("Error in sendAudioWhatsAppMessage:", error);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to send WhatsApp message.",
+        error: error.message,
+      });
+    });
+});
+
+app.get("/sendVideoWhatsAppMessage", (req, res) => {
+  sendVideoWhatsAppMessage()
+    .then((getData) => {
+      res.json({
+        status: "success",
+        message: "Webhook received and data fetch initiated.",
+        data: getData.data.messages,
+      });
+    })
+    .catch((error) => {
+      console.error("Error in sendVideoWhatsAppMessage:", error);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to send WhatsApp message.",
+        error: error.message,
+      });
+    });
+});
+
+// Upload the filw to whatsapp and get the file ID and then share to customer whatspp
+app.get("/sendMediaWhatsAppfiles", async (req, res) => {
+  const filePath = process.cwd() + "/media/test.webp";
+  console.log("File Path:", filePath);
+  await sendMediaWhatsAppfiles(filePath)
+    .then(async (getData) => {
+      if (getData?.data) {
+        console.log("getData 2");
+
+        let mediaType = await fileTypeFromFile(filePath);
+        console.log("File Extension index:", mediaType?.mime.split("/")[0]);
+
+        sendImageWhatsAppIDMessage(
+          getData?.data?.id,
+          mediaType?.mime.split("/")[0]
+        ).then((dbRes) => {
+          console.log("Image sent successfully with ID message response:");
+          res.json({
+            status: "success",
+            message: "Webhook received and data fetch initiated.",
+            data: getData.data,
+          });
+        });
+        // res.json({
+        //   status: "success",
+        //   message: "Webhook received and data fetch initiated.",
+        //   data: getData.data,
+        // });
+      }
+    })
+    .catch((error) => {
+      console.error("Error in sendMediaWhatsfiles:", error);
       res.status(500).json({
         status: "error",
         message: "Failed to send WhatsApp message.",
